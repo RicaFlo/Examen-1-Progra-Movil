2049e34294a468c38a35dcb109579ddbf878d661
Ricardo Alexander Flores Pineda <ricaflo124@gmail.com>
2024-08-12 20:11:49 -0600

    Pagina de Noticias + Botones de todas las paginas en el Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,8 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => HomeScreen(),
+        '/news': (context) => NewsScreen(),
+        
       },
     );
   }
@@ -32,11 +34,80 @@ class HomeScreen extends StatelessWidget {
                 color: Colors.blue,
               ),
             ),
+            ListTile(
+              leading: Icon(Icons.newspaper),
+              title: Text('Noticias'),
+              onTap: () {
+                Navigator.pushNamed(context, '/news');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('El cambio de Monedas'),
+              onTap: () {
+                Navigator.pushNamed(context, '/currency');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.list),
+              title: Text('Lista de tareas'),
+              onTap: () {
+                Navigator.pushNamed(context, '/tasks');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.speaker_notes),
+              title: Text('Podcast'),
+              onTap: () {
+                Navigator.pushNamed(context, '/podcast');
+              },
+            ),
           ],
         ),
       ),
       body: Center(
         child: Text('Bienvenido a la App CEUTEC'),
+      ),
+    );
+  }
+}
+class NewsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Noticias'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: Column(
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.article),
+                  title: Text('Título de la noticia'),
+                  subtitle: Text('Descripción breve de la noticia.'),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text('Detalles adicionales de la noticia.'),
+                ),
+                ButtonBar(
+                  children: <Widget>[
+                    TextButton(
+                      child: const Text('BUTTON 1'),
+                      onPressed: () { /* ... */ },
+                    ),
+                    TextButton(
+                      child: const Text('BUTTON 2'),
+                      onPressed: () { /* ... */ },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
