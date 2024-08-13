91c75f1c44c8777049ae579c05a60ed2e832b1ff
Ricardo Alexander Flores Pineda <ricaflo124@gmail.com>
2024-08-12 19:59:33 -0600

    Menu Inicial + Drawer

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'App CEUTEC',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('App CEUTEC'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Menu'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Text('Bienvenido a la App CEUTEC'),
+      ),
+    );
+  }
+}
